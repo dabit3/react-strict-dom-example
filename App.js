5239ed005eca3b09cc1914f08960e7eb41fc302c
nader <dabit3@gmail.com>
2024-02-21 12:14:20 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { css, html } from 'react-strict-dom';
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Failed prop type'])
+
+const styles = css.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopWidth: 1
+  },
+  h1: { padding: 10 },
+  button: {
+    borderRadius: 20,
+    backgroundColor: '#700fad',
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50
+  },
+  buttonText: { 
+    color: 'white',
+    fontWeight: 'bold'
+  }
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <html.div style={styles.container}>
+      <html.h1 style={styles.h1}>Hello World</html.h1>
+      <html.button
+        style={styles.button}
+        onClick={() => alert('Hello World')}
+      >
+        <html.p
+          style={styles.buttonText}
+        >
+          Click me
+        </html.p>
+      </html.button>
+    </html.div>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,15 @@
+## Example of React Strict Dom
+
+Build mobile apps with HTML
+
+Barebones example of how to integrate [React Strict Dom](https://github.com/facebook/react-strict-dom/tree/main)
+
+See `app.json` for library-specific configuration.
+
+### Getting started
+
+1. Clone the repo
+
+2. Install the dependencies
+
+3. Run the app
